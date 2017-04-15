921b60d6aa50c40abf128a236c48d1310767fa31
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-04-15 01:20:49 -0500

    feat(Domain): create especial layer for domain approach

--- common/models/order.js
+++ common/models/order.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const AddressEntity = require('loopback').getModel('AddressEntity');
+const OrderEntity = require('loopback').getModel('OrderEntity');
+
 module.exports = function (Order) {
 
+    Order.create = (order, cb) => {
+        AddressEntity.create(order.address, (err, address) => {
+            order.address = address;
+
+            OrderEntity.create(order, cb);
+        });
+    };
+
+    Order.remoteMethod('create', {
+        accepts: {arg: 'order', type: 'Order', required: true, http: {source: 'body'}},
+        returns: {arg: 'order', type: 'Order', "root": true},
+        http: {path: '/', verb: 'post'}
+    });
 };
