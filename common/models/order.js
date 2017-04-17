c7f3c5a7257591713a398690a4c948cabb2fd1e1
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-04-16 22:40:07 -0500

    feat(Models): create method for Order model is finished

--- common/models/order.js
+++ common/models/order.js
@@ -5,17 +5,29 @@ const OrderEntity = require('loopback').getModel('OrderEntity');
 
 module.exports = function (Order) {
 
-    Order.create = (order, cb) => {
-        AddressEntity.create(order.address, (err, address) => {
-            order.address = address;
+  Order.create = (order, cb) => {
+    OrderEntity.beginTransaction('READ COMMITTED', (err, tx) => {
+      const addressEntity = new AddressEntity(order.address);
 
-            OrderEntity.create(order, cb);
-        });
-    };
+      AddressEntity.create(addressEntity, {transaction: tx}, (err, addressEntity) => {
+        const orderEntity = new OrderEntity(order);
 
-    Order.remoteMethod('create', {
-        accepts: {arg: 'order', type: 'Order', required: true, http: {source: 'body'}},
-        returns: {arg: 'order', type: 'Order', "root": true},
-        http: {path: '/', verb: 'post'}
+        orderEntity.address(addressEntity);
+
+        OrderEntity.create(orderEntity, {transaction: tx}, (err, orderEntity) => tx.commit((err) => {
+          const order = new Order(orderEntity);
+
+          order.address = addressEntity;
+
+          cb(err, order);
+        }));
+      });
     });
+  };
+
+  Order.remoteMethod('create', {
+    accepts: {arg: 'order', type: 'Order', required: true, http: {source: 'body'}},
+    returns: {arg: 'order', type: 'Order', "root": true},
+    http: {path: '/', verb: 'post'}
+  });
 };
