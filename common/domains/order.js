4959d66aa146b836455e564f747db96e88fe3b63
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-04-18 17:02:31 -0500

    chore(Domains): move address and order to domain package

--- common/domains/order.js
+++ common/domains/order.js
@@ -3,9 +3,25 @@
 const AddressEntity = require('loopback').getModel('AddressEntity');
 const OrderEntity = require('loopback').getModel('OrderEntity');
 
-module.exports = function (Order) {
+module.exports = (Order) => {
 
-  Order.create = (order, cb) => {
+  /*
+  * Business constraints
+  * */
+
+  Order.validatesLengthOf('name', {'min': 3, 'max': 15});
+  Order.validatesNumericalityOf('latitude', {'int': false});
+  Order.validatesNumericalityOf('longitude', {'int': false});
+  Order.validatesFormatOf('latitude', {'with': /^(\+|-)?(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$/});
+  Order.validatesFormatOf('longitude', {'with': /^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$/});
+
+  /*
+  * Business methods
+  * */
+
+  Order.createOrder = (order, cb) => {
+    if (!order.isValid()) cb(order.errors);
+
     OrderEntity.beginTransaction('READ COMMITTED', (err, tx) => {
       const addressEntity = new AddressEntity(order.address);
 
@@ -25,24 +41,34 @@ module.exports = function (Order) {
     });
   };
 
-  Order.findById = (idOrder, cb) => OrderEntity.findById(idOrder, {include: 'address'}, cb);
+  Order.findOrderById = (idOrder, cb) => OrderEntity.findById(idOrder, {include: 'address'}, cb);
 
-  Order.find = (filter, cb) => OrderEntity.find(filter, cb);
+  Order.findOrders = (filters, cb) => {
+    const query = filters || JSON.parse(filters);
 
-  Order.remoteMethod('create', {
+    Object.assign({include: 'address', limit: 10}, query);
+
+    OrderEntity.find(query, cb);
+  };
+
+  /*
+   * Remote API methods
+   * */
+
+  Order.remoteMethod('createOrder', {
     accepts: {arg: 'order', type: 'Order', required: true, http: {source: 'body'}},
     returns: {arg: 'order', type: 'Order', root: true},
     http: {path: '/', verb: 'post'}
   });
 
-  Order.remoteMethod('findById', {
+  Order.remoteMethod('findOrderById', {
     accepts: {arg: 'idOrder', type: 'string', required: true, http: {source: 'path'}},
     returns: {arg: 'order', type: 'Order', root: true},
     http: {path: '/:idOrder', verb: 'get'}
   });
 
-  Order.remoteMethod('find', {
-    accepts: {arg: 'filter', type: 'string', required: true, http: {source: 'query'}},
+  Order.remoteMethod('findOrders', {
+    accepts: {arg: 'filter', type: 'string', required: false, http: {source: 'query'}},
     returns: {arg: 'orders', type: 'array'},
     http: {path: '/', verb: 'get'}
   });
