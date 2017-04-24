9d948f786e9f9f533614595e3f0a1536a6f52004
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-04-23 22:02:12 -0500

    feat(Server): add persisted process for ACL, User and RoleMapper models

--- server/boot/model.js
+++ server/boot/model.js
@@ -1,10 +1,26 @@
-module.exports = function(app) {
+module.exports = (app) => {
 
-  app.dataSources.db.automigrate('OrderEntity', function(err) {
+  app.dataSources.db.automigrate('User', (err) => {
     if (err) throw err;
   });
 
-  app.dataSources.db.automigrate('AddressEntity', function(err) {
+  app.dataSources.db.automigrate('AccessToken', (err) => {
+    if (err) throw err;
+  });
+
+  app.dataSources.db.automigrate('ACL', (err) => {
+    if (err) throw err;
+  });
+
+  app.dataSources.db.automigrate('RoleMapping', (err) => {
+    if (err) throw err;
+  });
+
+  app.dataSources.db.automigrate('OrderEntity', (err) => {
+    if (err) throw err;
+  });
+
+  app.dataSources.db.automigrate('AddressEntity', (err) => {
     if (err) throw err;
   });
 };
