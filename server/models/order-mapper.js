d6efc9f23754a421e31d17d497093faa5a38cd63
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-04-23 22:01:25 -0500

    refactor(Models): apply better configuration for OrderMapper

--- server/models/order-mapper.js
+++ server/models/order-mapper.js
@@ -2,31 +2,33 @@ const ObjectMapper = require('two-way-object-mapper');
 
 module.exports = (OrderMapper) => {
 
-  OrderMapper.build = (() => {
-    return new ObjectMapper()
-      .addPropertyMapping({from: 'id', to: 'id'})
-      .addPropertyMapping({from: 'name', to: 'name'})
-      .addPropertyMapping({from: 'latitude', to: 'latitude'})
-      .addPropertyMapping({from: 'longitude', to: 'longitude'})
-      .addPropertyMapping({from: 'status', to: 'status'})
-      .addPropertyMapping({from: 'timeZone', to: 'timeZone'})
-      .addPropertyMapping({from: 'scheduled', to: 'scheduled'})
-      .addPropertyMapping({from: 'comments', to: 'comments'})
-      .addPropertyMapping({from: 'total', to: 'total'})
-      .addPropertyMapping({from: 'createdAt', to: 'createdAt'})
-      .addPropertyMapping({from: 'registeredAt', to: 'registeredAt', default: null})
-      .addPropertyMapping({from: 'scheduledAt', to: 'scheduledAt', default: null})
-      .addPropertyMapping({from: 'finishedAt', to: 'finishedAt', default: null})
-      .addPropertyMapping({from: 'address.id', to: 'address.id'})
-      .addPropertyMapping({from: 'address.intNumber', to: 'address.intNumber'})
-      .addPropertyMapping({from: 'address.extNumber', to: 'address.extNumber'})
-      .addPropertyMapping({from: 'address.block', to: 'address.block'})
-      .addPropertyMapping({from: 'address.number', to: 'address.number'})
-      .addPropertyMapping({from: 'address.street', to: 'address.street'})
-      .addPropertyMapping({from: 'address.colony', to: 'address.colony'})
-      .addPropertyMapping({from: 'address.municipality', to: 'address.municipality'})
-      .addPropertyMapping({from: 'address.state', to: 'address.state'})
-      .addPropertyMapping({from: 'address.country', to: 'address.country'});
-  })()
+  const mapper = new ObjectMapper()
+    .addPropertyMapping({from: 'id', to: 'id'})
+    .addPropertyMapping({from: 'name', to: 'name'})
+    .addPropertyMapping({from: 'latitude', to: 'latitude'})
+    .addPropertyMapping({from: 'longitude', to: 'longitude'})
+    .addPropertyMapping({from: 'status', to: 'status'})
+    .addPropertyMapping({from: 'timeZone', to: 'timeZone'})
+    .addPropertyMapping({from: 'scheduled', to: 'scheduled'})
+    .addPropertyMapping({from: 'comments', to: 'comments'})
+    .addPropertyMapping({from: 'total', to: 'total'})
+    .addPropertyMapping({from: 'createdAt', to: 'createdAt'})
+    .addPropertyMapping({from: 'registeredAt', to: 'registeredAt', default: null})
+    .addPropertyMapping({from: 'scheduledAt', to: 'scheduledAt', default: null})
+    .addPropertyMapping({from: 'finishedAt', to: 'finishedAt', default: null})
+    .addPropertyMapping({from: 'address.id', to: 'address.id'})
+    .addPropertyMapping({from: 'address.intNumber', to: 'address.intNumber'})
+    .addPropertyMapping({from: 'address.extNumber', to: 'address.extNumber'})
+    .addPropertyMapping({from: 'address.block', to: 'address.block'})
+    .addPropertyMapping({from: 'address.number', to: 'address.number'})
+    .addPropertyMapping({from: 'address.street', to: 'address.street'})
+    .addPropertyMapping({from: 'address.colony', to: 'address.colony'})
+    .addPropertyMapping({from: 'address.municipality', to: 'address.municipality'})
+    .addPropertyMapping({from: 'address.state', to: 'address.state'})
+    .addPropertyMapping({from: 'address.country', to: 'address.country'});
 
+  OrderMapper.map = (object) => mapper.map(object);
+  OrderMapper.mapList = (list) => list.map((object) => mapper.map(object));
+  OrderMapper.reverseMap = (object) => mapper.reverseMap(object);
+  OrderMapper.reverseMapList = (list) => list.map((object) => mapper.reverseMap(object));
 };
