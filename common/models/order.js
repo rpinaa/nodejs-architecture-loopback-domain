881d4e058709b9e38cf8beb56dd7d53bfc27542f
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-05-08 14:45:54 -0500

    feat(Domain): implement PageableMapper into findOrders method

--- common/models/order.js
+++ common/models/order.js
@@ -143,7 +143,7 @@ module.exports = Order => {
     async.waterfall([
       (callback) => {
 
-        const query = filters && JSON.parse(filters) || {};
+        const query = Order.app.models.PageableMapper.map(filters && JSON.parse(filters) || {});
 
         OrderEntity.find(query, callback);
       },
