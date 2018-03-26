e03ba3d7930cee52d49f7c604b91474db79cde86
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2018-03-26 00:36:32 -0600

    refactor(entity): update entities using async/await concepts removing callbacks

--- server/models/order-entity.js
+++ server/models/order-entity.js
@@ -1,5 +1,8 @@
-'use strict';
-
 module.exports = OrderEntity => {
 
+  OrderEntity.throwErrorIfNotExist = exist =>
+    new Promise((solve, reject) => exist ? reject('4001') : solve());
+
+  OrderEntity.throwErrorIfNone = order =>
+    new Promise((solve, reject) => order ? solve(order) : reject('4001'));
 };
